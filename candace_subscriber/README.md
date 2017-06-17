48f8a240245a057721e00259d6cf737925f5fbee
Bryce McDonnell <bryce@bridgetownint.com>
2017-06-17 18:15:53 -0400

    Adds a parent subscriber for my sister's publisher

---
+++ candace_subscriber/README.md
@@ -0,0 +1,5 @@
+# Parent Subscriber
+
+This project is used to alert parents when the daughter's door is open at night.
+
+It just uses the built in LED on the board and flashes when the door is opened.
---
+++ candace_subscriber/candace_subscriber.ino
@@ -0,0 +1,107 @@
+/*
+ * Notify parents when door has opened
+ */
+
+#include <ArduinoJson.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <PubSubClient.h>
+
+const char* ssid     = "iot-2.4G";
+const char* password = "i0tconnect";
+const char* mqtt_server = "mqtt02.thedevranch.net";
+const char* doorjamb_topic = "beavdoorjamb";
+const char* subscriber_name = "lightsensor003";
+
+#define SENSOR_THRESHOLD_FOR_FLASHING 100
+#define DELAY_BETWEEN_FLASHES 100
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(BUILTIN_LED, HIGH); // turn off the light to start
+  Serial.begin(115200);
+  /* NETWORK SET UP */
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  WiFi.enableAP(0);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  } 
+  client.loop();
+  //delay(1000); // wait 1s before publishing another value
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect(subscriber_name)) {
+      client.subscribe(doorjamb_topic);
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& jsonRoot = jsonBuffer.parseObject(payload);
+  
+  if (!jsonRoot.success()) {
+    Serial.println("parseObject() failed");
+    return;
+  }
+
+  int lightValue = jsonRoot["lightValue"];
+  Serial.print("light: ");
+  Serial.println(lightValue);
+  
+  // Switch on the LED if an 1 was received as first character
+  if (lightValue > SENSOR_THRESHOLD_FOR_FLASHING) {
+   flashLED();
+  }
+}
+
+void flashLED() {
+  int flashTimes = 5;
+  while(flashTimes > 0) {
+    Serial.print(" ");
+    Serial.print(flashTimes);
+    digitalWrite(BUILTIN_LED, LOW);
+    delay(DELAY_BETWEEN_FLASHES);
+    digitalWrite(BUILTIN_LED, HIGH);
+    delay(DELAY_BETWEEN_FLASHES);
+    flashTimes--;
+  }
+  Serial.println();
+}
+
