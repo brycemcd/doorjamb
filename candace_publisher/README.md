ae097aade93ea94abc6ee071ed2e370d966b239b
Bryce McDonnell <bryce@bridgetownint.com>
2017-06-17 16:50:09 -0400

    Created light sensor publisher for my sister

---
+++ candace_publisher/README.md
@@ -0,0 +1,8 @@
+# DOORJAMB PUBLISHER
+
+The purpose of this repo is to sense when the door is open or closed and
+publish the value of the hall effects sensor to a central message
+broker.
+
+Consumers of the broker can do various things in response to the events
+published.
---
+++ candace_publisher/candace_publisher.ino
@@ -0,0 +1,85 @@
+/*
+ * Reads photoresistor and magnetic read switch
+ * Publishes json to queue with status of ambient light
+ * in the room and status of door (open or closed)
+ */
+#include <ArduinoJson.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <PubSubClient.h>
+
+const char* ssid     = "iot-2.4G";
+const char* password = "i0tconnect";
+const char* mqtt_server = "mqtt02.thedevranch.net";
+const char* doorjamb_topic = "beavdoorjamb";
+const char* subscriber_name = "lightsensor002";
+
+const int sensorPin = A0;
+const int ledPin = 13;
+int lightValue = 0;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup() {
+  Serial.begin(115200);
+
+  /* NETWORK SET UP */
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  WiFi.enableAP(0);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  client.setServer(mqtt_server, 1883);
+}
+
+void loop() {
+  // RESET JSON
+  StaticJsonBuffer<200> jsonBuffer; // increase the value of 200 if the json string gets larger
+  JsonObject& json = jsonBuffer.createObject();
+
+  if (!client.connected()) {
+    reconnect();
+  } else {
+    lightValue = analogRead(sensorPin);
+
+    json["lightValue"] = lightValue;
+
+    char postable[1024];
+    json.printTo(postable, sizeof(postable));
+
+    json.prettyPrintTo(Serial);
+    
+    client.publish(doorjamb_topic, postable );
+  }
+  client.loop();
+  delay(1000); // wait 1s before publishing another value
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect(subscriber_nameb)) {
+      Serial.println("connected");
+
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
